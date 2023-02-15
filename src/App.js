985a7f45fa68186523ec196dc3d1770ae3fe97e8
ducanhlaminh <89836104+ducanhlaminh@users.noreply.github.com>
2023-02-15 23:02:10 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./Componnets/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-screen flex justify-center">
+      <div className="max-w-[1700px]">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
